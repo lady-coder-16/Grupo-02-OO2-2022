8d6c2daafc8ea36b3275a5df5b00329a50964832
Matias <71100012+matiasFS@users.noreply.github.com>
2022-05-05 20:40:06 -0300

    Archivos iniciales

---
+++ src/main/java/com/trabajo/Grupo02OO22022/TestBCryptPasswordEncoder.java
@@ -0,0 +1,17 @@
+package com.trabajo.Grupo02OO22022;
+
+
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+public class TestBCryptPasswordEncoder {
+
+	public static void main(String[] args) {
+		
+//		 BCryptPasswordEncoder pe = new BCryptPasswordEncoder();
+//		 System.out.println(pe.encode("admin"));
+		
+
+		
+			
+	}
+}
