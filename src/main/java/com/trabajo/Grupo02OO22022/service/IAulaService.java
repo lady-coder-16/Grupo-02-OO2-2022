3b551c6a38952de6b1f8f89bf552abd673ef17fd
Matias Silvestri <71100012+matiasFS@users.noreply.github.com>
2022-05-19 21:40:50 -0300

    Falto este archivo xd

--- src/main/java/com/trabajo/Grupo02OO22022/service/IAulaService.java
+++ src/main/java/com/trabajo/Grupo02OO22022/service/IAulaService.java
@@ -2,12 +2,15 @@ package com.trabajo.Grupo02OO22022.service;
 
 import java.util.List;
 import com.trabajo.Grupo02OO22022.entity.Aula;
+import com.trabajo.Grupo02OO22022.entity.Laboratorio;
+import com.trabajo.Grupo02OO22022.entity.Tradicional;
 
 public interface IAulaService {
 
     public List <Aula> listaAulas();
     public void guardar (Aula aula);
-    public Aula buscarPorID (long id);
+    public Tradicional buscarPorID (long id);
+    public Laboratorio buscarPorIDLab (long id);
 
     
 }
