6411a0c56c625d4442475027390da32c32ab4c4b
Matias Silvestri <71100012+matiasFS@users.noreply.github.com>
2022-05-19 21:40:20 -0300

    Traer aula por id

--- src/main/java/com/trabajo/Grupo02OO22022/repository/AulaRepository.java
+++ src/main/java/com/trabajo/Grupo02OO22022/repository/AulaRepository.java
@@ -3,6 +3,8 @@ package com.trabajo.Grupo02OO22022.repository;
 import java.util.List;
 
 import com.trabajo.Grupo02OO22022.entity.Aula;
+import com.trabajo.Grupo02OO22022.entity.Laboratorio;
+import com.trabajo.Grupo02OO22022.entity.Tradicional;
 
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
@@ -12,6 +14,5 @@ import org.springframework.stereotype.Repository;
 public interface AulaRepository extends CrudRepository<Aula, Long> {
 
     List<Aula> findAll();
- 
     
 }
--- src/main/java/com/trabajo/Grupo02OO22022/service/AulaServiceImplements.java
+++ src/main/java/com/trabajo/Grupo02OO22022/service/AulaServiceImplements.java
@@ -3,7 +3,11 @@ package com.trabajo.Grupo02OO22022.service;
 import java.util.List;
 
 import com.trabajo.Grupo02OO22022.entity.Aula;
+import com.trabajo.Grupo02OO22022.entity.Laboratorio;
+import com.trabajo.Grupo02OO22022.entity.Tradicional;
 import com.trabajo.Grupo02OO22022.repository.AulaRepository;
+import com.trabajo.Grupo02OO22022.repository.LaboratorioRepository;
+import com.trabajo.Grupo02OO22022.repository.TradicionalRepository;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
@@ -15,6 +19,12 @@ public class AulaServiceImplements implements IAulaService {
 
     @Autowired
     private AulaRepository aulaRepository;
+
+    @Autowired
+    private TradicionalRepository tradicionalRepository;
+
+    @Autowired
+    private LaboratorioRepository laboratorioRepository;
 
     @Override
     public List<Aula> listaAulas() {
@@ -28,10 +38,26 @@ public class AulaServiceImplements implements IAulaService {
     }
 
     @Override
-    public Aula buscarPorID(long id) { 
-        return aulaRepository.findById(id).orElse(null);
+    public Tradicional buscarPorID(long id) { 
+            List<Tradicional> tradicional  = tradicionalRepository.findAll();
+            Tradicional tradicional1 = new Tradicional();
+            for(Tradicional tradicional2 : tradicional) {
+                if(tradicional2.getId()==id) {
+                    tradicional1 = tradicional2;
+                }
+            }
+            return tradicional1;
     }
    
-
-    
+    @Override
+    public Laboratorio buscarPorIDLab(long id) { 
+            List<Laboratorio> laboratorio  = laboratorioRepository.findAll();
+            Laboratorio laboratorio1 = new Laboratorio();
+            for(Laboratorio laboratorio2 : laboratorio) {
+                if(laboratorio2.getId()==id) {
+                    laboratorio1 = laboratorio2;
+                }
+            }
+            return laboratorio1;
+    }
 }
